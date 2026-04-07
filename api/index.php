e1f59f88e4b286594cdbf1bb18d923bae2c43d80
andhyabernaz <andhyabernaz@gmail.com>
2026-04-07 09:20:21 +0700

    Mitigate MySQL prepared statement errors

--- api/index.php
+++ api/index.php
@@ -115,7 +115,7 @@ function pdo(array $env): PDO {
   $db = new PDO($dsn, $user, $pass, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES => true,
   ]);
   return $db;
 }
--- cpanel-public/api/index.php
+++ cpanel-public/api/index.php
@@ -113,7 +113,7 @@ function pdo(array $env): PDO {
   $db = new PDO($dsn, $user, $pass, [
     PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::ATTR_EMULATE_PREPARES => false,
+    PDO::ATTR_EMULATE_PREPARES => true,
   ]);
   
   // Reset session and clear prepared statement cache
